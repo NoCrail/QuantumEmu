fdcfb2a46f83e3abb76ae1ce4824bcba544cc625
Владислав <vlad-s-d@ya.ru>
2021-04-06 16:43:14 +0300

    Initial Commit

---
+++ QuantumEmu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuantumEmu
+//
+//  Created by Vladislav Dunaev on 06.04.2021.
+//  Copyright © 2021 Vladislav Dunaev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
